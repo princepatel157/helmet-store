e7db43f4cd2b9c79323060c0b723add24e1237d9
Prince Patel <princepatel1576@gmail.com>
2021-01-29 16:39:11 +0530

    checkout page

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,27 @@
 import React from "react";
 import "./Checkout.css";
+import Subtotal from "./Subtotal.js";
 
 function Checkout() {
-  return <div>this is checkout page</div>;
+  return (
+    <>
+      <div className="line">
+        <hr />
+      </div>
+      <div className="container checkout">
+        <div className="row">
+          <div className="col-lg-6 checkout_left">
+            <div className="checkout_heading">
+              <h2 className="checkout_title">Your Shopping Basket</h2>
+            </div>
+          </div>
+          <div className="col-lg-4 col-lg-offset-2 checkout_right">
+            <Subtotal />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Checkout;
