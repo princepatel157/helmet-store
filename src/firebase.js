d8bc39e8ed0592e41f7f1f0a6d1c4195612e80e3
Prince Patel <princepatel1576@gmail.com>
2021-01-30 12:13:05 +0530

    setting up firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAvLp7S38bx_MO0yG32g-ae7j3zCST7Gco",
+  authDomain: "ashi-helmets.firebaseapp.com",
+  projectId: "ashi-helmets",
+  storageBucket: "ashi-helmets.appspot.com",
+  messagingSenderId: "31717289115",
+  appId: "1:31717289115:web:aa1d8974f995fda6a0ea3e",
+  measurementId: "G-ZDL74L6XZW",
+};
